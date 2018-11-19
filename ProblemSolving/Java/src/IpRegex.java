9a866fe94516a0a8e8ebfe9c2ca5a2b9643301ac
xtra <nickvanhoof1@gmail.com>
2018-11-19 23:34:21 +0100

    solve IpRegex leading zeros allowed java

---
+++ ProblemSolving/Java/src/IpRegex.java
@@ -0,0 +1,33 @@
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IpRegex {
+
+    public static void main(String[] args) {
+
+        String patternStr = "^((000|[0-9]|[0-9][0-9]|[0-1][0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[0-9][0-9]|[0-1][0-9]{2}|2[0-4][0-9]|25[0-5])$";
+        Pattern pattern = Pattern.compile(patternStr);
+
+        String input = "this fails";
+
+        // create a matcher that will match the given input against this pattern
+        Matcher matcher = pattern.matcher(input);
+
+        boolean matchFound = matcher.matches();
+        System.out.println(input + " - matches: " + matchFound);
+
+        input = "this passes the test";
+        // reset the matcher with a new input sequence
+        matcher.reset(input);
+        matchFound = matcher.matches();
+        System.out.println(input + " - matches: " + matchFound);
+
+        // Attempts to match the input sequence, starting at the beginning
+        // of the region, against the pattern
+        matchFound = matcher.lookingAt();
+        System.out.println(input + " - matches from the beginning: " + matchFound);
+
+    }
+
+}
